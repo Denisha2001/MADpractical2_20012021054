e463f4693e354c947a1b693d520df25b7aef7e0b
Denisha2001 <savaliyadenisha143@gmail.com>
2022-08-26 11:22:46 +0530

    2lk

---
+++ app/src/main/java/com/example/madpractical2_20012021054/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.madpractical2_20012021054
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private val TAG="MainActivity"
+    private  lateinit var mycl:ConstraintLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mycl=findViewById(R.id.mycl)
+        Log.i(TAG,"OnCreate function called")
+        Toast.makeText(this,"OnCreate function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnCreate function called.",Snackbar.LENGTH_LONG).show()
+
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"OnStart function called")
+        Toast.makeText(this,"OnStart function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnStart function called.",Snackbar.LENGTH_LONG).show()
+
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG,"OnResume function called")
+        Toast.makeText(this,"OnResume function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnResume function called.",Snackbar.LENGTH_LONG).show()
+
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG,"OnRestart function called")
+        Toast.makeText(this,"OnRestart function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnRestart function called.",Snackbar.LENGTH_LONG).show()
+
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"OnPause function called")
+        Toast.makeText(this,"OnPause function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnCPause function called.",Snackbar.LENGTH_LONG).show()
+
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"OnStop function called")
+        Toast.makeText(this,"OnStop function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnStop function called.",Snackbar.LENGTH_LONG).show()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"OnDestroy function called")
+        Toast.makeText(this,"OnDestroy function called.",Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"OnDestroy function called.",Snackbar.LENGTH_LONG).show()
+
+    }
+}
